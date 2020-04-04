1b1d72f9f39be3cbbd664fb807bea524ba334f03
Maria Mihai <maria.ic.mihai@gmail.com>
2020-04-04 16:56:35 +0200

    Updated the get test to document a query parameter. This will be ignored as it isn't used by the controller. Added as example for the available capabilities. Closes #5

--- src/test/java/guru/springframework/sbmrestdocs/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sbmrestdocs/web/controller/BeerControllerTest.java
@@ -25,8 +25,7 @@ import static org.mockito.BDDMockito.given;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(RestDocumentationExtension.class)
@@ -48,11 +47,14 @@ class BeerControllerTest {
     void getBeerById() throws Exception {
         given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+                .param("iscold", "yes") // doesn't exist, just for example, will be ignored
+                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("v1/beer", pathParameters(
-                        parameterWithName("beerId").description("UUID of the desired beer to get")
-                )));
+                .andDo(document("v1/beer",
+                                pathParameters(parameterWithName("beerId").description("UUID of the desired beer to get")),
+                                requestParameters(parameterWithName("iscold").description("is Beer Cold Query param"))
+                       ));
     }
 
     @Test
